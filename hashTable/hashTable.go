6c9be1c1f70d768d28ebf7da9127da78bdd16245
xueTP <1493177371@qq.com>
2019-01-13 23:06:16 +0800

    hash表先提交

---
+++ hashTable/hashTable.go
@@ -0,0 +1,72 @@
+package hashTable
+
+import (
+	"dataStruct/binaryTree"
+	"expvar"
+)
+
+const maxRestSize = 9
+const minRestSize = 1
+
+type HashTable struct {
+	size int
+	data []binaryTree.BinarySearchTreeToMap
+	hashKey int
+	hashSeed []int
+}
+
+func NewHashTable() *HashTable {
+	return &HashTable{
+		hashKey: 0,
+		data: make([]binaryTree.BinarySearchTreeToMap, 17),
+		hashSeed: []int{17, 37, 79, 163, 331, 673, 1361, 2729, 5471, 10949, 21911, 43853, 87719, 175447, 350899, 701819, 1403641, 2807303, 5614657, 11229331, 22458671, 44917381, 89834777, 179669557, 359339171, 718678369, 1437356741, 2147483647},
+	}
+}
+
+func (ht *HashTable) getHashCode(v interface{}) int64 {
+	return GetHashCode(v) % int64(ht.hashSeed[ht.hashKey])
+}
+
+func (ht *HashTable) IsEmpty() bool {
+	return ht.size == 0
+}
+
+func (ht *HashTable) GetSize() int {
+	return ht.size
+}
+
+func (ht *HashTable) resetSize(newKey int) {
+	if newKey < 0 {
+		newKey = 0
+	}else if newKey > len(ht.hashSeed) - 1 {
+		newKey = len(ht.hashSeed) - 1
+	}
+	newData := make([]binaryTree.BinarySearchTreeToMap, newKey)
+	oldKey := ht.hashKey
+	ht.hashKey = newKey
+	for k, list := range ht.data {
+		for j, v := range list {
+
+		}
+	}
+}
+
+func (ht *HashTable) Set(k binaryTree.Compared, v interface{}) {
+	hashCode := ht.getHashCode(k)
+	ht.data[hashCode].AddNode(k, v)
+	ht.size ++
+}
+
+func (ht *HashTable) Get(k binaryTree.Compared) interface{} {
+	hashCode := ht.getHashCode(k)
+	return ht.data[hashCode].GetVal(k)
+}
+
+func (ht *HashTable) Del(k binaryTree.Compared) error {
+	hashCode := ht.getHashCode(k)
+	return ht.data[hashCode].DelNode(k)
+}
+
+
+
+
